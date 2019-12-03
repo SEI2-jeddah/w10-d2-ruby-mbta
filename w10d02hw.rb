b7662e361b9f59fac9f033b18c21b2426afc005a
Ahlam Fallatah <f18.ahlam@gmail.com>
2019-12-03 10:56:55 +0300

    start solving homework

---
+++ w10d02hw.rb
@@ -0,0 +1,6 @@
+class Subway
+    def Stops(startline, startstation, endline, endstation)
+end
+stops_between_stations('Red', 'Alewife', 'Red', 'Alewife') # 0
+stops_between_stations('Red', 'Alewife', 'Red', 'South Station') # 7
+stops_between_stations('Red', 'South Station', 'Green', 'Kenmore') # 6
