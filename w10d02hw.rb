5fe4e493e1f9b63165661e3c7957eee6f26df6a6
Ahlam Fallatah <f18.ahlam@gmail.com>
2019-12-07 11:29:24 +0300

    Done

--- w10d02hw.rb
+++ w10d02hw.rb
@@ -1,6 +1,26 @@
 class Subway
     def Stops(startline, startstation, endline, endstation)
-end
-stops_between_stations('Red', 'Alewife', 'Red', 'Alewife') # 0
-stops_between_stations('Red', 'Alewife', 'Red', 'South Station') # 7
-stops_between_stations('Red', 'South Station', 'Green', 'Kenmore') # 6
+
+    lines={
+    red: [ "South Station" ,"Park Street" , "Kendall" ,"Central" , "Harvard" , "Porter" , "Davis" ,"Alewife"],
+    green: ["Government Center", "Park Street", "Boylston", "Arlington","Copley","Hynes","Kenmore" ],
+    orange: ["North Station", "Haymarket", "Park Street", "State", "Downtown Crossing","Chinatown","Back Bay","Forest Hills"]
+    } 
+       
+    steps = 0
+      if startline == endline
+        puts (lines[startline.to_sym].index(endstation)) - (lines[startline.to_sym].index(startstation))
+      end 
+
+      if startline!=endline 
+        steps = (lines[startline.to_sym].index("Park Street")) - (lines[startline.to_sym].index(startstation))
+        steps += (lines[endline.to_sym].index(endstation)) - (lines[endline.to_sym].index("Park Street"))
+        puts steps
+      end 
+    end
+end   
+
+# stops_between_stations('Red', 'Alewife', 'Red', 'Alewife') # 0
+# stops_between_stations('Red', 'Alewife', 'Red', 'South Station') # 7
+# stops_between_stations('Red', 'South Station', 'Green', 'Kenmore') # 6
+
